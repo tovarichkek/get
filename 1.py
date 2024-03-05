e76d02b73e00cc3a08cccb28c92ba7c9abce13f1
tovarich_kek <nikolaevn2005@ya.ru>
2024-03-05 13:16:56 +0300

    python scripts

---
+++ 1.py
@@ -0,0 +1,20 @@
+a = "0.456	0.518	0.639	0.557	0.859	0.807	1.072	1.096	1.760	1.497"
+									
+b = "0.171	0.220	0.335	0.255	0.607	0.535	0.944	0.986	2.547	1.841"
+
+
+
+
+
+
+
+
+for i in (a.split()):
+	print(i, end = " & ")
+print()
+for i in (b.split()):
+	print(i, end = " & ")
+print()
+for i in (c.split()):
+	print(i, end = " & ")
+print()
